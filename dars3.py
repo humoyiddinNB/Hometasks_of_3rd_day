08aee09d634d386a81bfb60043807d7ec6bfd861
humoyiddinNB <defendereviver71@gmail.com>
2024-12-19 19:10:36 +0500

    second task added

--- dars3.py
+++ dars3.py
@@ -1,15 +1,35 @@
 import os
 os.system("cls")
 
+
 ######      20-masala
-# def ask_numbers():
-#     a = int(input("How many numbers will u insert?"))
-#     nums =  [int(input(f"Insert {i} - number: ")) for i in range(1, a + 1)]
-#     return nums
+# def ask_nums():
+#     a = int(input("How many numbers will u add: "))
+#     result = [int(input(f"Insert {i + 1} - number: ")) for i in range(a)]
+#     return result
 
 # def find_little_num(a):
 #     result = [a[i] for i in range(0, len(a) - 1) if a[i] < a[i + 1]]
 #     return result
 
-# nums = ask_numbers()
+# nums = ask_nums()
 # print(find_little_num(nums))
+
+
+
+
+
+
+
+
+#######     21-masala
+# def ask_nums():
+#     num = int(input("How many nums will you insert? "))
+#     result = [int(input(f"Insert {i + 1} - number: ")) for i in range(num)]
+#     return result
+
+# def checker(a):
+#     return sorted(a) == a
+
+# nums = ask_nums()
+# print(checker(nums))

b8481b32391a2ea84674dfb49581810ccd9702fd
humoyiddinNB <defendereviver71@gmail.com>
2024-12-19 18:31:52 +0500

    1-task added

---
+++ dars3.py
@@ -0,0 +1,15 @@
+import os
+os.system("cls")
+
+######      20-masala
+# def ask_numbers():
+#     a = int(input("How many numbers will u insert?"))
+#     nums =  [int(input(f"Insert {i} - number: ")) for i in range(1, a + 1)]
+#     return nums
+
+# def find_little_num(a):
+#     result = [a[i] for i in range(0, len(a) - 1) if a[i] < a[i + 1]]
+#     return result
+
+# nums = ask_numbers()
+# print(find_little_num(nums))
